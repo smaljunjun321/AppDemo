9816146d2215657fea55d06470f6c4938253ec8c
lenovo <lenovo@192.168.200.82>
2017-03-30 10:13:14 +0800

    测试提交github

---
+++ testDemo/src/testDemo/TestCase.java
@@ -0,0 +1,13 @@
+package testDemo;
+
+public class TestCase {
+	public static void main(String[] args) {
+		String dd="1111";
+		testString(dd);
+		System.out.println(dd);
+	}
+	
+	public static void testString(String dd){
+		dd="444";
+	}
+}
